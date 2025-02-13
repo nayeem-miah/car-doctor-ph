a12af55309e3161af847aa63df872e98aabd6187
nayeem-miah <nayeem5113a@gmail.com>
2025-02-13 16:19:47 +0600

    about done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import About from "@/components/HomePage/About";
+import Banner from "@/components/HomePage/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    home page
+    <Banner/>
+    <About/>
    </div>
   );
 }
