f01d8269fee624eb907c7891fe6af55a619831ef
nayeem-miah <nayeem5113a@gmail.com>
2025-03-01 17:36:01 +0600

    creating sign in and signup page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,75 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+
+
+const Login = () => {
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+ 
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-1/2 mr-12">
+          <Image src="/assets/images/login/login.svg" alt="" width={400} height={400} className="rounded-xl" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-3xl text-center font-bold">Login</h1>
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <p className="my-4 text-center">
+              New to Car Doctors{" "}
+              <Link  className="text-orange-600 font-bold" href="/signup">
+                Sign Up
+              </Link>{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
