9aa727ba2ce2bb64928457a3d7e85c7830f21afe
nayeem-miah <nayeem5113a@gmail.com>
2025-03-01 17:15:43 +0600

    loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+const loading =()=>{
+    return (
+        <div className="py-20 text-4xl text-red-500 text-center">
+           <h4 className="text-center"> loading ...........</h4>
+        </div>
+    )
+}
+export default loading;
