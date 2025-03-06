a295c827f04db2ade2e3165bdd2c48d8c8e981c7
nayeem-miah <nayeem5113a@gmail.com>
2025-03-06 14:31:20 +0600

    hash password

--- src/app/signup/api/route.js
+++ src/app/signup/api/route.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/lib/connectDB";
+import bcrypt from "bcrypt";
 
 export const POST = async(request)=>{
     const newUser = await request.json();
@@ -9,7 +10,8 @@ export const POST = async(request)=>{
         if(existingUser){
             return Response.json({message : "user already exists"}, {status : 304});
         }
-        const result = await userCollection.insertOne(newUser);
+        const hashPassword = bcrypt.hashSync(newUser.password, 14);
+        const result = await userCollection.insertOne({...newUser, password : hashPassword});
         // console.log(result);
         return Response.json({message : "user created successfully"}, {status : 200});
 }catch(e){
